5298f8efaa42f8dd64321a9682d3ad9571965f8b
心叶 <yelloxing@gmail.com>
2017-09-25 09:08:09 +0800

    2.0版本初始化

--- src/script.js
+++
@@ -1,7 +0,0 @@
-function toggleCheckbox(target) {
-    var nameHook = {
-        "notSelect": "select",
-        "select": "notSelect"
-    };
-    target.setAttribute('name', nameHook[target.getAttribute('name')]);
-}

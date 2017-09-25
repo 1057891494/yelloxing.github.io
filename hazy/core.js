3d755850bdb8d23000917b36e58306090bf52a43
心叶 <yelloxing@gmail.com>
2017-09-25 13:28:57 +0800

    Hazy对象（除了字符串类型）

--- hazy/core.js
+++ hazy/core.js
@@ -2,7 +2,102 @@
 var Hazy = function(selector, context) {
     return new Hazy.prototype.init(selector, context);
 };
+/**
+ * @param selector [string,function,dom,Hazy Object] 选择器
+ * @param context [dom,Hazy Object] 查找上下文，默认document
+ *
+ * @return Hazy Object
+ * {
+ *  [],//查找的结果保存在数组中
+ *  context,//查找时使用的上下文
+ *  length,//查找回来的个数
+ *  isTouch//返回是否是已经查找过的结点
+ *  selector//选择器
+ * }
+ */
 Hazy.prototype.init = function(selector, context, root) {
+
+    /**
+     * 初始化参数
+     */
+    root = root || rootHazy;
+    context = context ? Hazy(context)[0] : document;
+    this.length = 0;
+    this.context = context;
+    this.selector = selector;
+    if (!selector) {
+        return this;
+    }
+
+    /**
+     * 分类处理
+     */
+
+    //1.body比较特殊，直接提出来
+    if (selector == "body") {
+        this.context = document;
+        this[0] = document.body;
+        this.isTouch = true;
+        this.length = 1;
+        return this;
+    }
+
+    //2.如果是字符串
+    if (typeof selector === 'string') {
+        return this;
+    }
+
+    //3.如果是DOM节点
+    if (selector.nodeType === 1 || selector.nodeType === 11 || selector.nodeType === 9) {
+        this.context = context;
+        this[0] = selector;
+        this.isTouch = true;
+        this.length = 1;
+        return this;
+    }
+
+    //4.如果是function
+    if (typeof selector === 'function') {
+        if (Hazy.__isLoad__) {
+            selector();
+        } else {
+            if (document.addEventListener) {
+                //Mozilla, Opera and webkit
+                document.addEventListener("DOMContentLoaded", function doListenter() {
+                    document.removeEventListener("DOMContentLoaded", doListenter, false);
+                    selector();
+                    Hazy.__isLoad__ = true;
+                });
+
+            } else if (document.attachEvent) {
+                //IE
+                document.attachEvent("onreadystatechange", function doListenter() {
+                    if (document.readyState === "complete") {
+                        document.detachEvent("onreadystatechange", doListenter);
+                        selector();
+                        Hazy.__isLoad__ = true;
+                    }
+                });
+
+            }
+        }
+        return this;
+    }
+
+    //5.如果是Hazy对象
+    if (selector.isTouch) {
+        this.isTouch = true;
+        var flag = 0;
+        for (; flag < selector.length; flag++) {
+            this[flag] = selector[flag];
+        }
+        this.context = selector.context || context;
+        this.length = selector.length;
+        this.selector = selector.selector;
+        return this;
+    }
+
+    return this;
 
 };
 var rootHazy = Hazy(document);

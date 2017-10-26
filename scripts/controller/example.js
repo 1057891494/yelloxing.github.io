4c94028e4349f6c1eca0cf9c1c0ba584194440a8
心叶 <yelloxing@gmail.com>
2017-10-26 10:44:51 +0800

    添加Tranform+Transitions+Animation用例

--- scripts/controller/example.js
+++ scripts/controller/example.js
@@ -1,17 +1,21 @@
 Hazy.controller("ExampleCtrl", function($scope) {
 
     //初始化代码
-    $scope.initCode = `
-        <script>
-            /*这里你可以写脚步*/
-        </script>
-        <style>
-            /*这里填写你要的样式*/
-        </style>
-        <div class='unique-example'>
-            <!--你可以在这里写例子代码！-->
-        </div>
-    `;
+    $scope.initCode =
+        `<script>
+/*这里你可以写脚步*/
+
+</script>
+
+<style>
+/*这里填写你要的样式*/
+
+</style>
+
+<div class='unique-demo'>
+<!--你可以在这里写例子代码！-->
+
+</div>`;
 
     //应用代码
     $scope.apply = function(code) {

e70fc9b8de35b67a5a50d0ca7da15cc99ffb44c3
心叶 <yelloxing@gmail.com>
2018-01-12 23:23:02 +0800

    添加复制功能

--- script/_script.js
+++ script/_script.js
@@ -1,6 +1,17 @@
 window.bookContentDom = undefined;
 window.contentDom = undefined;
 window.bookname = "";
+window.doClipboard = function(node) {
+    window.clipboard(node.parentNode.innerText);
+};
+window.addClipboard = function() {
+    var codeNodes = document.getElementsByClassName('code');
+    var flag, codeNode;
+    for (flag = 0; flag < codeNodes.length; flag++) {
+        codeNode = codeNodes[flag];
+        window.append(codeNode, window.stringToDom('<div class="clipboard" onclick="doClipboard(this)"></div>'));
+    }
+};
 window.openBook = function(url) {
     window.bookname = url.split(/\//)[1] + "/" + url.split(/\//)[2];
     window.location.hash = url.split(/\//)[1] + "/" + url.split(/\//)[2];
@@ -32,6 +43,7 @@ window.openContent = function(target, url) {
     }
     window.doAjax(url, function(data) {
         window.contentDom.innerHTML = data;
+        window.addClipboard();
         var script = window.contentDom.getElementsByTagName('script');
         if (script.length > 0) {
             window["eval"].call(window, script[0].innerText);
--- script/_tool.js
+++ script/_tool.js
@@ -9,7 +9,20 @@ window.prepend = function(targetNode, node) {
     } else {
         throw new Error("Not acceptable type!");
     }
-}
+};
+
+/**
+ * 在被选元素内部的结尾插入内容
+ */
+window.append=function(targetNode, node){
+    if (node.nodeType === 1 || node.nodeType === 11 || node.nodeType === 9) {
+        targetNode.appendChild(node);
+    } else if (typeof node == 'string') {
+        targetNode.appendChild(stringToDom(node));
+    } else {
+        throw new Error("Not acceptable type!");
+    }
+};
 
 /**
  * 字符串变成dom,必须有一个大的包裹
@@ -18,7 +31,7 @@ window.stringToDom = function(nodeString) {
     var frameDiv = document.createElement("div");
     frameDiv.innerHTML = nodeString;
     return frameDiv.childNodes[0];
-}
+};
 
 
 /**
@@ -27,4 +40,14 @@ window.stringToDom = function(nodeString) {
 window.remove = function(node) {
     var $parent = node.parentNode;
     $parent.removeChild(node);
-}
+};
+
+/**
+ * 把指定文字复制到剪切板
+ */
+window.clipboard = function(text) {
+    window.prepend(document.getElementsByTagName('body')[0], window.stringToDom('<textarea id="clipboard-textarea">' + text + '</textarea>'));
+    document.getElementById("clipboard-textarea").select();
+    document.execCommand("copy", false, null);
+    window.remove(document.getElementById('clipboard-textarea'));
+};

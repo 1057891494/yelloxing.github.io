be1945b91b5357b7de2deb6834619dadfe3c6645
心叶 <yelloxing@gmail.com>
2018-01-14 14:59:01 +0800

    修复移动端复制赋值内容的显示问题

--- script/_tool.js
+++ script/_tool.js
@@ -46,7 +46,7 @@ window.remove = function(node) {
  * 把指定文字复制到剪切板
  */
 window.clipboard = function(text, callback, errorback) {
-    window.prepend(document.getElementsByTagName('body')[0], window.stringToDom('<textarea id="clipboard-textarea">' + text + '</textarea>'));
+    window.prepend(document.getElementsByTagName('body')[0], window.stringToDom('<textarea id="clipboard-textarea" style="position:absolute">' + text + '</textarea>'));
     document.getElementById("clipboard-textarea").select();
     try {
         document.execCommand("copy", false, null);

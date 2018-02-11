fb2bd75a10dad5e5d3bd32473670a2d635ca0708
心叶 <yelloxing@gmail.com>
2018-02-11 14:37:06 +0800

    ###恢复复制

--- script/root.js
+++ script/root.js
@@ -1,5 +1,15 @@
-window.Core = {};
-require('./_ajax.js');
-require('./_dom.js');
-require('./_tool.js');
-require('./_script.js');
+/*! yelloxing.github.io V4 | (c) 2007, 2018 心叶 | Apache-2.0 https://github.com/yelloxing/yelloxing.github.io.git */
+
+// 引入Luna.js
+require('../node_modules/luna-library/luna.js');
+
+$(function() {
+    $('pre.code').append('<div class="clipboard"></div>');
+    $('div.clipboard').bind('click', function() {
+        $.clipboard($(this).parent().text(), function() {
+            alert('复制成功，现在可以粘贴了.');
+        }, function() {
+            alert('非常抱歉，复制失败！');
+        });
+    });
+});
